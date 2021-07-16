ecfcc63524280df57b79aebd683407c59fb979eb
developersth <kritsadee.satewin@gmail.com>
2021-07-17 00:38:53 +0700

    add sample index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'static',
+  target: 'server',
   serverMiddleware: ['~/server/index.js'],
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000'
