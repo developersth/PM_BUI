facee0d69af63d79c6d991a4669c1b75477f8622
developersth <69202290+developersth@users.noreply.github.com>
2021-08-29 03:55:07 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ import colors from 'vuetify/es5/util/colors'
 let BASE_URL = "http://ktd-dev.ddns.net:5000"
 let Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic3ViIjoiYWRtaW5fZGV2IiwidXNlcm5hbWUiOiJhZG1pbiIsInBhc3N3b3JkIjoiYWRtaW4iLCJpYXQiOjE2Mjk5NzU1MjJ9.d5iyYlbdQYgEOFZMe_Ax38gpQ9IM0VTAUwnDtM5AiHk"
 export default {
-  mode: 'spa',
   build: {
     extend(config, ctx) {
       if (ctx.isDev) {
